b3d71883cdc3bccc4803e88b8a1e8ea452311b55
Fakhira Zahra Zulfira <35962477+firafakhira@users.noreply.github.com>
2018-12-01 08:22:45 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8012/Tubes_fix';
+$config['base_url'] = 'http://localhost/Tubes_fix';
 
 /*
 |--------------------------------------------------------------------------
